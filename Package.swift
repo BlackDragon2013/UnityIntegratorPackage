cc4eb8070e7a0a6f37b6d690e31c51b9501bfdf5
Omar Barrera Peña <o-barrera@hotmail.com>
2022-10-20 09:38:50 -0500

    added dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UnityIntegratorPackage",
-            dependencies: []),
+            dependencies: ["UnityFramework"]),
         .binaryTarget(
             name: "UnityFramework",
             path: "Frameworks/UnityFramework.xcframework"),
